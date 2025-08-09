9e6bd3032027212e41750ddacbe8f41098b47d90
DHARSHAN KESAVAN POTTI <dharshankesavanpotti@mac.mynetworksettings.com>
2025-08-08 22:29:07 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      <Navbar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
